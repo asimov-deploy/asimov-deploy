f31b0aaa6835c0a9ba736097a153dcebff9d2649
Torkel <torkel.odegaard@gmail.com>
2012-12-18 15:39:22 +0100

    removed unused code

--- src/AsimovDeploy.WinAgentUpdater/Updater.cs
+++ src/AsimovDeploy.WinAgentUpdater/Updater.cs
@@ -153,25 +153,7 @@ namespace AsimovDeploy.WinAgentUpdater
             foreach (DirectoryInfo subDirectory in dir.GetDirectories()) subDirectory.Delete(true);
         }
 
-        private static void CopyDirectory(string sourcePath, string destPath)
-        {
-            if (!Directory.Exists(destPath))
-            {
-                Directory.CreateDirectory(destPath);
-            }
-
-            foreach (string file in Directory.GetFiles(sourcePath))
-            {
-                string dest = Path.Combine(destPath, Path.GetFileName(file));
-                File.Copy(file, dest);
-            }
-
-            foreach (string folder in Directory.GetDirectories(sourcePath))
-            {
-                string dest = Path.Combine(destPath, Path.GetFileName(folder));
-                CopyDirectory(folder, dest);
-            }
-        }
+       
     }
 
    
