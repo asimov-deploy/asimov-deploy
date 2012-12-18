6258ff6ec1d95aa46bc0be475fe9cb4dd65119f8
Torkel <torkel.odegaard@gmail.com>
2012-12-18 10:55:14 +0100

    changed copyright in sharedassembly.info

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Ebay Sweden")]
-[assembly: AssemblyCopyright("© Ebay Sweden 2012. All rights reserved.")]
+[assembly: AssemblyCompany("Ebay Inc.")]
+[assembly: AssemblyCopyright("© Ebay Inc. 2012. All rights reserved.")]
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("{version}")]
