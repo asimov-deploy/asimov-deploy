f944f62c923bcc9730c4f83f2bc10d83d5c3b1dd
Torkel <torkel.odegaard@gmail.com>
2012-12-19 16:44:47 +0100

    began adding license header to src files

--- src/AsimovDeploy.NodeFront/public/app/loadbalancer/loadbalancer.js
+++ src/AsimovDeploy.NodeFront/public/app/loadbalancer/loadbalancer.js
@@ -1,3 +1,19 @@
+/*******************************************************************************
+* Copyright (C) 2012 eBay Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************************/
+
 define([
     "jquery",
     "backbone",
@@ -6,7 +22,7 @@ define([
     "./settings-view"
 ],
 function($, Backbone, app, HostListView, SettingsView) {
-   
+
     var HostStatus = Backbone.Model.extend({
     });
 
@@ -19,7 +35,7 @@ function($, Backbone, app, HostListView, SettingsView) {
 
     // public api
     var loadbalancer = {};
-   
+
     app.vent.on("agent:event:loadBalancerStateChanged", function(data) {
         var host = hosts.get(data.id);
         host.set({enabled: data.enabled});
