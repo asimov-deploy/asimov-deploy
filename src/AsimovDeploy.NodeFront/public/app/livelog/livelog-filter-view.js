1e5df039a09833a897d2ad82188a49ae4d15a5ce
Torkel Ödegaard <torkel.odegaard@gmail.com>
2013-01-15 12:46:33 +0100

    live log filter toggling works

---
+++ src/AsimovDeploy.NodeFront/public/app/livelog/livelog-filter-view.js
@@ -0,0 +1,64 @@
+/*******************************************************************************
+* Copyright (C) 2012 eBay Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************************/
+
+define([
+   "jquery",
+   "underscore",
+   "backbone",
+   "marionette",
+   "app"
+],
+function($, _, Backbone, Marionette, app) {
+
+  var LiveLogFilterSelectionViewItem = Marionette.ItemView.extend({
+      template: "live-log-filter-item",
+      tagName: "li",
+      events: {
+         "click": "filterToggle"
+      },
+
+      initialize: function(){
+         this.model.on('change', this.render, this);
+      },
+
+      filterToggle: function() {
+         var show = this.model.get('show') ? false : true;
+         this.model.set({show: show});
+
+         $(this.el).toggleClass('show-filter-enabled', show);
+
+         this.trigger("filterToggle");
+      }
+
+   });
+
+   return Marionette.CollectionView.extend({
+      itemView: LiveLogFilterSelectionViewItem,
+
+      initialize: function() {
+         this.on("itemview:filterToggle", this.filterToggle, this);
+      },
+
+      filterToggle: function() {
+         var agents = this.collection.where({show: true});
+         var names = agents.length === 0 ? this.collection.pluck(["id"]) : _.pluck(agents, ["id"]);
+
+         app.trigger("livelog:filterUpdated", names);
+      }
+
+   });
+
+});
---
+++ src/AsimovDeploy.NodeFront/public/app/livelog/livelog-list-view.js
@@ -0,0 +1,68 @@
+/*******************************************************************************
+* Copyright (C) 2012 eBay Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+******************************************************************************/
+
+define([
+   "jquery",
+   "underscore",
+   "backbone",
+   "marionette",
+   "app"
+],
+function($, _, Backbone, Marionette, app) {
+
+   var LogItemView = Marionette.ItemView.extend({
+      template: "agent-log-item"
+   });
+
+   return Backbone.View.extend({
+
+      events: {
+         "dblclick": "toggleExpand"
+      },
+
+      initialize: function(){
+
+         app.vent.on("agent:log", function(logItems) {
+            var models = _.map(logItems, function(item) { return new Backbone.Model(item); });
+            var views = _.map(models, function(m) { return new LogItemView({model: m}); });
+
+            var fragment = document.createDocumentFragment();
+            _.each(views, function(view) {
+               view.render();
+               fragment.appendChild(view.el);
+            });
+
+            this.$el.append(fragment);
+            this.$el.scrollTop(this.$el[0].scrollHeight);
+
+         }, this);
+      },
+
+      toggleExpand: function(){
+         var current = Math.round($(this.el).height()) + "px";
+         var min = $(this.el).css("min-height");
+         var max = $(this.el).css("max-height");
+
+         if (current == min) {
+            $(this.el).animate({ height: max}, 500);
+         } else {
+            $(this.el).animate({ height: min}, 500);
+         }
+      }
+
+   });
+
+});
