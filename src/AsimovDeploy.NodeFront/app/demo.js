f2a403ed9f58a0b8446968ac2748e0bc9bd37a7a
Torkel Ödegaard <torkel.odegaard@gmail.com>
2013-07-01 10:35:57 +0200

    worked on demo (heroku) mode

--- src/AsimovDeploy.NodeFront/app/demo.js
+++ src/AsimovDeploy.NodeFront/app/demo.js
@@ -18,6 +18,26 @@
 module.exports = function(server) {
 
 	var demodata = require('./demodata/demo-data-generator.js');
+	var _ = require('underscore');
+
+	function emitLog(agentName, message) {
+		var currentTime = new Date();
+
+		var logs = [
+			{
+				agentName: agentName,
+				time: "" + currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds(),
+				level: "info",
+				message: message
+			}
+		];
+
+		clientSockets.sockets.volatile.emit('agent:log', logs);
+	}
+
+	function emitAgentEvent(data) {
+		clientSockets.sockets.volatile.emit('agent:event', data);
+	}
 
 	server.get('/agents/list', function(req, res) {
 		res.json(demodata.agents);
@@ -25,6 +45,43 @@ module.exports = function(server) {
 
 	server.get('/units/list', function(req, res) {
 		res.json(demodata.units);
+	});
+
+	server.get("/agent/query", function(req, res) {
+		if (req.query.url.indexOf('MyCoolWebApp') !== -1) {
+			return res.json(demodata.versions["MyCoolWebApp.com"]);
+		}
+		if (req.query.url.indexOf('WebAPI') !== -1) {
+			console.log("WebApi!" + req.query.url);
+			return res.json(demodata.versions["MyCoolWebApp.com WebAPI"]);
+		}
+		if (req.query.url.indexOf('Backend') !== -1) {
+			console.log("Backend!");
+			return res.json(demodata.versions["Backend.QueueHandler"]);
+		}
+	});
+
+	server.post("/loadbalancer/change", function(req, res) {
+		res.json("ok");
+
+		req.body.hosts.forEach(function(host) {
+			console.log("Hosts!" + host.id);
+			var agent = _.find(demodata.agents, function(agent) {
+				return agent.loadBalancerId === host.id;
+			});
+
+			agent.loadBalancerEnabled = !agent.loadBalancerEnabled;
+
+			emitAgentEvent({
+				eventName: "loadBalancerStateChanged",
+				id: host.id,
+				enabled: agent.loadBalancerEnabled
+			});
+
+			//setTimeout(function() {
+			//	emitLog(agent.name, agent.loadBalancerEnabled ? "Load balancer state enabled (accessible)" : "Load balancer state disabled (inaccessible)");
+			//	}, 500);
+		});
 	});
 
 };
