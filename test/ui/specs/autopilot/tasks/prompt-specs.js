150d82f449dd19aa0dccac7cfc2ae3434ce070e4
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2016-04-24 21:33:00 +0200

    Prompt task specs updated

--- test/ui/specs/autopilot/tasks/prompt-specs.js
+++ test/ui/specs/autopilot/tasks/prompt-specs.js
@@ -6,10 +6,12 @@ define([
     'autopilot/tasks/Prompt'
 ], function (Backbone, sequence, app, PromptTask) {
     describe("Autopilot", function() {
+
         describe("Tasks", function() {
+
             describe("Prompt", function() {
 
-                describe("given autopilot non-paused", function() {
+                describe("given autopilot not paused and executing prompt task", function() {
                     var config = {
                         paused: false
                     };
@@ -18,55 +20,63 @@ define([
                     var spy = jasmine.createSpy('event');
                     vent.on('autopilot:pause-deploy', spy);
 
-                    describe("when executing prompt task", function() {
-                        var task = new PromptTask(config, vent);
-                        var promise = sequence([task.execute()])
-                            .then(function () {
-                                console.log('done');
-                            })
-                            .otherwise(function (e) {
-                                console.log('error', e);
-                            });
+                    var task = new PromptTask(config, vent);
+                    sequence([task.execute()]);
 
-                        it("should trigger pause deploy", function() {
-                            setTimeout(function () {
-                                expect(spy).toHaveBeenCalled();
-                            }, 1);
-                        });
+                    it("should trigger pause deploy", function () {
+                        setTimeout(function () {
+                            expect(spy).toHaveBeenCalled();
+                        }, 1);
                     });
                 });
 
-                describe("given autopilot paused", function() {
-                    var config = {
-                        paused: true
-                    };
+                describe("given autopilot paused and executing prompt task", function() {
+                    var vent;
+                    var spy;
+                    var promise;
 
-                    var vent = new Backbone.Wreqr.EventAggregator();
-                    var spy = jasmine.createSpy('event');
-                    vent.on('autopilot:pause-deploy', spy);
+                    beforeEach(function () {
+                        var config = {
+                            paused: true
+                        };
 
-                    var task = new PromptTask(config, vent);
-                    var promise = sequence([task.execute()]);
+                        vent = new Backbone.Wreqr.EventAggregator();
+                        spy = jasmine.createSpy('event');
+                        vent.on('autopilot:pause-deploy', spy);
 
-                    describe("when executing prompt task", function() {
+                        var task = new PromptTask(config, vent);
+                        promise = sequence([task.execute()]);
+                    });
 
-                        it("should not trigger pause deploy", function() {
-                            expect(spy).not.toHaveBeenCalled();
-                        });
+                    it("should not trigger pause deploy", function() {
+                        expect(spy).not.toHaveBeenCalled();
+                    });
 
-                        describe("and then triggering continue deploy", function() {
+                    describe("when triggering continue deploy", function() {
+                        beforeEach(function () {
                             setTimeout(function () {
                                 vent.trigger('autopilot:continue-deploy');
                             }, 1);
+                        });
 
-                            it("task should finish", function() {
-                                setTimeout(function () {
-                                    expect(promise.inspect().state).toBe('fulfilled');
-                                }, 1);
-                            });
+                        it("task should finish", function (done) {
+                            promise.then(done);
+                        });
+                    });
+
+                    describe("when triggering abort deploy", function() {
+                        beforeEach(function () {
+                            setTimeout(function () {
+                                vent.trigger('autopilot:abort-deploy');
+                            }, 1);
+                        });
+
+                        it("task should abort", function (done) {
+                            promise.otherwise(done);
                         });
                     });
                 });
+
             });
         });
     });
