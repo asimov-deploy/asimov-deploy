34c0d3c92703684d7ca808214225c12b92e438ed
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2017-09-07 21:02:53 +0200

    Only build a build-container and run tests using the build-container script

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -ev
-
 rm -rf docker-build
 
 docker build -t asimov-deploy/asimov-deploy:build . -f Dockerfile.build
@@ -19,10 +17,3 @@ cp package.json docker-build
 cp server.js docker-build
 cp LICENSE docker-build
 cp NOTICE docker-build
-
-docker build -t asimov-deploy/asimov-deploy:latest .
-
-PACKAGE_VERSION=$(sed -nE 's/^\s*"version": "(.*?)",$/\1/p' package.json)
-
-echo Building asimov-deploy/asimov-deploy:$PACKAGE_VERSION
-docker build -t asimov-deploy/asimov-deploy:$PACKAGE_VERSION .
