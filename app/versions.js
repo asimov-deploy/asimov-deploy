34c4696fe3676382af72840bb227abd257047869
Patrik Akelsson <pakselsson@paypal.com>
2018-08-17 12:09:10 +0200

    Forward deploylogs from agents to node front
    Previously, the deploy logs were served directly from the agents
    This commit changes that so that the deploy logs are served from the nodefront instead

--- app/versions.js
+++ app/versions.js
@@ -1,30 +1,40 @@
 /*******************************************************************************
-* Copyright (C) 2012 eBay Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-******************************************************************************/
+ * Copyright (C) 2012 eBay Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 
 var querystring = require("querystring");
 
-module.exports = function(app, config) {
+module.exports = function (app, config) {
+    var agentApiClient = require('./services/agent-api-client').create(config);
 
-	app.get('/deploylog/file', app.ensureLoggedIn, function(req, res) {
+    app.get('/deploylog/file', app.ensureLoggedIn, function (req, res) {
 
-		var agent =  config.getAgent(req.query.agentName);
-		var unitName = querystring.escape(req.query.unitName);
+        var agent = config.getAgent(req.query.agentName);
+        var unitName = querystring.escape(req.query.unitName);
 
-		res.redirect(agent.url + '/deploylog/file/' + unitName + "/" + req.query.position);
-
-	});
+        agentApiClient.getDeployLog(
+            req.query.agentName,
+            req.query.unitName,
+            req.query.position,
+            function (data) {
+                if(data == null){
+                    res.status(404).end();
+                    return;
+                }
+                res.type('text').send(data);
+            })
+    });
 
 };
